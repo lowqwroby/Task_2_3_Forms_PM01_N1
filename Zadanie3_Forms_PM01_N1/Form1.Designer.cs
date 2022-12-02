1782abff5ec6bb231fbe9896ddce4d98dbb30116
SaveLuciferR <cmudrec@mail.ru>
2022-12-02 22:29:20 +0300

    Добавьте файлы проекта.

---
+++ Zadanie3_Forms_PM01_N1/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace Zadanie3_Forms_PM01_N1
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox6 = new System.Windows.Forms.TextBox();
+			this.textBox7 = new System.Windows.Forms.TextBox();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.label9 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(34, 77);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(200, 22);
+			this.textBox1.TabIndex = 0;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(34, 105);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(200, 22);
+			this.textBox2.TabIndex = 1;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(261, 77);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(255, 22);
+			this.textBox3.TabIndex = 2;
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(261, 105);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(255, 22);
+			this.textBox4.TabIndex = 3;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 80);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(16, 16);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "A";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 108);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(16, 16);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "B";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(243, 105);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(16, 16);
+			this.label3.TabIndex = 6;
+			this.label3.Text = "Y";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(243, 77);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(15, 16);
+			this.label4.TabIndex = 7;
+			this.label4.Text = "X";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label5.Location = new System.Drawing.Point(9, 9);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(221, 16);
+			this.label5.TabIndex = 8;
+			this.label5.Text = "Введите границы диапазона";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label6.Location = new System.Drawing.Point(246, 9);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(276, 48);
+			this.label6.TabIndex = 9;
+			this.label6.Text = "Введите числа для определения, на\r\nкакие цифры будут оканчиваться\r\nчисла из диапа" +
+    "зона";
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(12, 161);
+			this.textBox5.Multiline = true;
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(505, 39);
+			this.textBox5.TabIndex = 10;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(412, 338);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(105, 23);
+			this.button1.TabIndex = 11;
+			this.button1.Text = "Определить";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// textBox6
+			// 
+			this.textBox6.Location = new System.Drawing.Point(12, 283);
+			this.textBox6.Multiline = true;
+			this.textBox6.Name = "textBox6";
+			this.textBox6.Size = new System.Drawing.Size(505, 39);
+			this.textBox6.TabIndex = 12;
+			// 
+			// textBox7
+			// 
+			this.textBox7.Location = new System.Drawing.Point(12, 222);
+			this.textBox7.Multiline = true;
+			this.textBox7.Name = "textBox7";
+			this.textBox7.Size = new System.Drawing.Size(505, 39);
+			this.textBox7.TabIndex = 13;
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(9, 142);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(225, 16);
+			this.label7.TabIndex = 14;
+			this.label7.Text = "Вычисление с помощью цикла For";
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Location = new System.Drawing.Point(9, 203);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(239, 16);
+			this.label8.TabIndex = 15;
+			this.label8.Text = "Вычисление с помощью цикла While";
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Location = new System.Drawing.Point(9, 264);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(253, 16);
+			this.label9.TabIndex = 16;
+			this.label9.Text = "Вычисление с помощью цила Do While";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(557, 379);
+			this.Controls.Add(this.label9);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.textBox7);
+			this.Controls.Add(this.textBox6);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox5);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox6;
+		private System.Windows.Forms.TextBox textBox7;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Label label9;
+	}
+}
+
---
+++ Zadanie3_Forms_PM01_N1/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadanie3_Forms_PM01_N1
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				int A = int.Parse(textBox1.Text);
+				int B = int.Parse(textBox2.Text);
+
+				int X = int.Parse(textBox3.Text);
+				int Y = int.Parse(textBox4.Text);
+
+				int i = A;
+
+				for(int j = A; j < B; j++)
+				{
+					if (Math.Abs(j) % 10 == X || Math.Abs(j) % 10 == Y)
+					{
+						textBox5.Text += string.Format("{0} ", j);
+					}
+				}
+
+				while(i < B)
+				{
+					if (Math.Abs(i) % 10 == X || Math.Abs(i) % 10 == Y)
+					{
+						textBox6.Text += string.Format("{0} ", i);
+					}
+					i++;
+				}
+
+				i = A;
+				do
+				{
+					if (Math.Abs(i) % 10 == X || Math.Abs(i) % 10 == Y)
+					{
+						textBox7.Text += string.Format("{0} ", i);
+					}
+					i++;
+				} while (i < B);
+			}
+			catch
+			{
+				textBox5.Text = string.Format("Некорректный ввод данных");
+			}
+		}
+	}
+}
